e80732129673dc92ba029e20d90403a79906f714
sohaibaltrefe <sohaibshaltafaltrefe1@gmail.com>
2024-05-21 08:24:39 +0300

    sgow category

---
+++ BookStore/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; } = null!;
+
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public DateTime UpdateOn { get; set; } = DateTime.Now;
+    }
+}
